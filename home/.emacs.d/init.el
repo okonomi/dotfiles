716f122188d83aba6d3fec1b57ff06828fe421b0
kawakami <kawakami@natsumeatari.co.jp>
2014-09-26 22:21:56 +0900

    init-loader 導入

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -2,5 +2,9 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
+(require 'init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/.emacs.d/inits")
+
 (load-theme 'molokai t t)
 (enable-theme 'molokai)
