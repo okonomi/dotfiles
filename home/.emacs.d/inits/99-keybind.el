70e2fb3b0308a2e1a387aef526486ce9547239bb
okonomi <oknm@okonomi.sakura.ne.jp>
2014-09-28 19:18:12 +0900

    キーバインド設定

---
+++ home/.emacs.d/inits/99-keybind.el
@@ -0,0 +1,11 @@
+;; C-hでバックスペース
+(define-key global-map "\C-h" 'delete-backward-char)
+
+;; C-oで動的略語展開
+(define-key global-map "\C-o" 'dabbrev-expand)
+
+;; コメントアウト
+(define-key global-map "\C-c;" 'comment-region)
+
+;; コメント解除
+(define-key global-map "\C-c:" 'uncomment-region)
