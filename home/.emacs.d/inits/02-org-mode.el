0ce2ad8c8062fa1619b3473dabfa635fd8a3b01a
okonomi <oknm@okonomi.sakura.ne.jp>
2014-10-15 11:22:03 +0900

    add org-mode setting

---
+++ home/.emacs.d/inits/02-org-mode.el
@@ -0,0 +1,23 @@
+(setq org-todo-keywords '("TODO" "STARTED" "|" "DONE")
+   org-todo-interpretation 'sequence)
+
+;; http://passingloop.tumblr.com/post/10150860851/org-clock-in-if-starting
+;; http://wp-nhoriguchi.rhcloud.com/?p=87
+(defun sacha/org-clock-in-if-starting ()
+  "Clock in when the task is marked STARTED."
+  (when (and (string= org-state "STARTED")
+             (not (string= org-last-state org-state)))
+    (org-clock-in)))
+(add-hook 'org-after-todo-state-change-hook
+	  'sacha/org-clock-in-if-starting)
+(defadvice org-clock-in (after sacha activate)
+  "Set this task's status to 'STARTED'."
+  (org-todo "STARTED"))
+
+(defun sacha/org-clock-out-if-waiting ()
+  "Clock in when the task is marked STARTED."
+  (when (and (string= org-state "WAITING")
+             (not (string= org-last-state org-state)))
+    (org-clock-out)))
+(add-hook 'org-after-todo-state-change-hook
+	  'sacha/org-clock-out-if-waiting)
