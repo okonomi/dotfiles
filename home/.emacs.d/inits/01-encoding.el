b3d3258e94ecedde4dff9a2fce4033cbd1797008
okonomi <oknm@okonomi.sakura.ne.jp>
2015-01-27 14:01:58 +0900

    エンコーディングを設定

---
+++ home/.emacs.d/inits/01-encoding.el
@@ -0,0 +1,8 @@
+;; http://yohshiy.blog.fc2.com/blog-entry-273.html
+
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
