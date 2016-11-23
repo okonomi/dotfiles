3a9a1ad2cd698d1fc5db8a4dd87bc573496f6e37
okonomi <oknm@okonomi.sakura.ne.jp>
2016-11-23 15:45:36 +0900

    カスタマイズ用のファイルを用意

---
+++ home/.emacs.d/custom_settings.el
@@ -0,0 +1,8 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-newline ((t (:foreground "gray21" :weight normal))))
+ '(whitespace-space ((t (:foreground "gray22"))))
+ '(whitespace-tab ((t (:foreground "gray22")))))
--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -7,3 +7,8 @@
 (init-loader-load "~/.emacs.d/inits")
 
 (load-theme 'dracula t)
+
+;; カスタマイズ用のファイルを設定
+(setq custom-file "~/.emacs.d/custom_settings.el")
+;; カスタマイズ用ファイルをロード
+(load custom-file t)
