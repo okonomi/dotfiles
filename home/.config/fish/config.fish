d64f905bc91aa24bd1248be8b9d8adc2ffe1389f
okonomi <okonomi@oknm.jp>
2018-12-08 14:49:42 +0900

    Goのパス設定

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -10,3 +10,6 @@ alias s='cd (ghq list --full-path | peco)'
 
 set PATH $HOME/.anyenv/bin $PATH
 status --is-interactive; and source (anyenv init -|psub)
+
+export GOPATH=$HOME/src
+set PATH $GOPATH/bin $PATH
