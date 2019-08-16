aee2e21632f7d35976c872276e53be6a7e99322c
okonomi <okonomi@oknm.jp>
2019-08-16 13:46:47 +0900

    change font

--- home/.emacs.d/custom_settings.el
+++ home/.emacs.d/custom_settings.el
@@ -6,3 +6,13 @@
  '(whitespace-newline ((t (:foreground "gray21" :weight normal))))
  '(whitespace-space ((t (:foreground "gray22"))))
  '(whitespace-tab ((t (:foreground "gray22")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(menu-bar-mode nil)
+ '(session-use-package t nil (session))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
--- home/.emacs.d/inits/cocoa-emacs-font.el
+++ home/.emacs.d/inits/cocoa-emacs-font.el
@@ -2,4 +2,4 @@
 ;; http://blog.kawauso.com/?p=617
 
 ; (add-to-list 'default-frame-alist '(font . "MigMix 1M"))
-(set-face-attribute 'default nil :family "Roboto Mono for Powerline" :height 200)
+; (set-face-attribute 'default nil :family "Roboto Mono for Powerline" :height 200)
