9a29f23e731dfa8f8ca20db010bbecc16652268b
okonomi <okonomi@oknm.jp>
2019-12-29 23:59:28 +0900

    fix path

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,14 +8,6 @@ end
 alias g='git'
 alias s='cd (ghq list --full-path | peco)'
 
-if type -q anyenv
-  status --is-interactive; and source (anyenv init -|psub)
-end
-
-if type -q /home/linuxbrew/.linuxbrew
-  eval (/home/linuxbrew/.linuxbrew/bin shellenv)
-end
-
 export GOPATH=$HOME
 
 set PATH $HOME/bin $PATH
@@ -23,6 +15,14 @@ set PATH $HOME/bin $PATH
 set PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+if type -q /home/linuxbrew/.linuxbrew
+  eval (/home/linuxbrew/.linuxbrew/bin shellenv)
+end
+
+if type -q anyenv
+  status --is-interactive; and source (anyenv init -|psub)
+end
 
 set -g theme_display_date no
 set -g theme_display_cmd_duration no
