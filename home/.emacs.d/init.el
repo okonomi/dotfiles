acc7e41c04c830faa99eb603be7859129d8288a5
okonomi <okonomi@oknm.jp>
2020-10-20 19:18:44 +0900

    add magit

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -90,3 +90,12 @@
   :ensure t
   :config
   (global-git-gutter-mode t))
+
+(leaf magit
+  :doc "A Git porcelain inside Emacs."
+  :req "emacs-25.1" "async-20200113" "dash-20200524" "git-commit-20200516" "transient-20200601" "with-editor-20200522"
+  :tag "vc" "tools" "git" "emacs>=25.1"
+  :added "2020-10-20"
+  :emacs>= 25.1
+  :ensure t
+  :after git-commit with-editor)
