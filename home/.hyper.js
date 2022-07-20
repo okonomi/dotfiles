45446533a80dee0d7fdb57eed944733887d0a41a
okonomi <okonomi@oknm.jp>
2022-07-20 15:21:41 +0900

    hyper: modify font

--- home/.hyper.js
+++ home/.hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: '"FiraCode Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"FiraCode Nerd Font", "Noto Mono for Powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
